a81d0ee6ce008e3c83331587861d2208f52662e4
gpofblank <metodiev.design@gmail.com>
2020-08-12 19:58:02 +0300

    added a start page and a todo interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TodoListPageComponent} from './todo/todo-list-page/todo-list-page.component';
+import {AppComponent} from './app.component';
+import {StartComponent} from './start/start.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: StartComponent},
+  {path: 'todo', component: TodoListPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListPageComponent } from './todo/todo-list-page/todo-list-page.component';
 import { TodoComponent } from './todo/todo/todo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListPageComponent,
     TodoComponent,
+    StartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
